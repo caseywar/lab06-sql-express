50bac121f4492d1bb45118931eb4856d67dd296f
Casey Warren <caseywar1017@gmail.com>
2021-02-19 19:23:16 -0500

    data scripts updated

---
+++ data/albums.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Channel Orange',
+    image: 'https://media.pitchfork.com/photos/5929be57c0084474cd0c2e8c/1:1/w_600/45e3c196.jpeg',
+    description: 'The debut studio album by American R&B singer and songwriter Frank Ocean',
+    category: 'Alternative R&B',
+    price: 10,
+
+  },
+  {
+    id: 2,
+    name: 'Man on the Moon III: The Chosen',
+    image: 'https://resources.tidal.com/images/0928ef0e/0eb4/464d/af75/72b38a3712d3/640x640.jpg',
+    description: 'The seventh studio album by American rapper Kid Cudi',
+    category: 'Hip Hop',
+    price: 20,
+  },
+  {
+    id: 3,
+    name: 'Mordechai',
+    image: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Khruangbin_Mordechai_Cover.png/220px-Khruangbin_Mordechai_Cover.png',
+    description: 'The third studio album by American musical trio Khruangbin',
+    category: 'Alternative/Indie',
+    price: 15,
+  },
+  {
+    id: 4,
+    name: 'Jazz for the Jet Set',
+    image: 'https://img.discogs.com/JXBhVIcnstwd2ZIp5aX6ZZ0RgvA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2756468-1345014171-2452.jpeg.jpg',
+    description: 'An album by American jazz vibraphonist Dave Pike',
+    category: 'Jazz',
+    price: 25,
+  },
+  {
+    id: 5,
+    name: 'Abacab',
+    image: 'https://progressivemusicplanet.files.wordpress.com/2016/07/115864557.jpg',
+    description: 'The eleventh studio album by English rock band Genesis',
+    category: 'Art Rock',
+    price: 20,
+  },
+  {
+    id: 6,
+    name: 'Morning View ',
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/68/Incubus_-_Morning_View.jpg',
+    description: 'The fourth studio album by American rock band Incubus',
+    category: 'Art Rock',
+    price: 15,
+  }
+];
